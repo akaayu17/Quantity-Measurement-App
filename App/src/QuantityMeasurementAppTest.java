6ff02dd1a70ad935b768bdb8aa16b7dd95d2a079
akaayu17 <kuikelaayusha7@gmail.com>
2026-05-05 10:18:48 +0530

    UC2 Completed

--- App/src/QuantityMeasurementAppTest.java
+++ App/src/QuantityMeasurementAppTest.java
@@ -47,4 +47,37 @@ public  class QuantityMeasurementAppTest {
         boolean output= f1.equals(f2);
         assertTrue(output);
     }
+
+    @Test
+    public void testInchesEquality_SameValue(){
+        QuantityMeasurementApp.Inches i1= new QuantityMeasurementApp.Inches(1.0);
+        QuantityMeasurementApp.Inches i2 =new QuantityMeasurementApp.Inches(1.0);
+        assertTrue(i1.equals(i2));
+    }
+
+    @Test
+    public void testInchesEquality_DifferentValue(){
+        QuantityMeasurementApp.Inches i1= new QuantityMeasurementApp.Inches(1.0);
+        QuantityMeasurementApp.Inches i2= new QuantityMeasurementApp.Inches(2.0);
+
+        assertFalse(i1.equals(i2));
+    }
+
+    @Test
+    public void testInchesEquality_Nullcomparison(){
+        QuantityMeasurementApp.Inches i1= new QuantityMeasurementApp.Inches(1.0);
+        assertFalse(i1.equals(null));
+    }
+
+    @Test
+    public void testInchesEquality_NonNumericInput(){
+        QuantityMeasurementApp.Inches i1= new QuantityMeasurementApp.Inches(1.0);
+        assertFalse(i1.equals("aaaa"));
+    }
+    @Test
+    public void testInchesEquality_SameRefernce(){
+        QuantityMeasurementApp.Inches i1=new QuantityMeasurementApp.Inches(1.0);
+        QuantityMeasurementApp.Inches i2= i1;
+        assertTrue(i1.equals(i2));
+    }
 }
